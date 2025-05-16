80c3c6e8498925b201afe594dd99370e51ea2554
Noelle Bhaduri <nmbhaduri@gmail.com>
2025-05-15 23:40:10 -0400

    Hall of Fame Updated

--- controllers/hallOfFameController.js
+++ controllers/hallOfFameController.js
@@ -1,11 +1,15 @@
 // File: /controllers/hallOfFameController.js
 // Purpose: Handle logic for Hall Of Fame page
 
+
 exports.showHallOfFame = (req, res) => {
   const dogs = [
-    { name: 'Buddy', breed: 'Golden Retriever', image: '/img/placeholder.png' },
-    { name: 'Luna', breed: 'Siberian Husky', image: '/img/placeholder.png' },
-    { name: 'Charlie', breed: 'Beagle', image: '/img/placeholder.png' }
+    { name: 'Arrow', breed: 'Australian Cattle Dog Mix', image: '/img/arrow1.jpg' },
+    { name: 'Arrow', breed: 'G.O.A.T.', image: '/img/arrow2.jpg' },
+    { name: 'Arrow', breed: 'Ever Expadning Force of Love', image: '/img/arrow3.jpg' },
+    { name: 'Peggy', breed: 'Arizona Desert Dog', image: '/img/peggy.jpg' },
+    { name: 'Stella', breed: 'Arizona Desert Dog', image: '/img/stella.jpg' },
+    { name: 'Sheena', breed: 'Boston Terrier', image: '/img/sheena.jpg' }
   ];
 
   res.render('pages/halloffame', { dogs, user: req.session.user || null, title: 'Hall of Fame' });
